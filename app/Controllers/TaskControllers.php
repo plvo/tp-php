70efba9dc4697ce6cfcc4096160d5aee2c816f67
plvo <talhatecim@gmail.com>
2025-02-18 14:34:03 +0100

    feat(app): add Task Model and Controller

---
+++ app/Controllers/TaskControllers.php
@@ -0,0 +1,15 @@
+<?php
+
+use App\Http\Controllers\Controller;
+use App\Models\Task;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class TaskController extends Controller
+{
+    public function index()
+    {
+        $tasks = Task::where('user_id', Auth::id())->get();
+        return view('tasks.index', compact('tasks'));
+    }
+}
